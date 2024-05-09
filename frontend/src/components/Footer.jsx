085ebf2fec6a1d849cd58555fb448a542292ab38
shauryatomer1 <shauryatomar1@gmail.com>
2024-05-09 14:49:12 +0530

    abc

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">SHAURYA</div>
+          <div className="right">
+            <p>RGIPT CANTEEN,JAIS</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            
+          </div>
+          <div className="right">
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
